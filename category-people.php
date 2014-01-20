435db2f3256b8feea12f9eec3fe16059fcdc2b34
Casey Ydenberg <ydenberg@gmail.com>
2014-01-19 19:43:07 -0500

    Fixed ordering of people

--- category-people.php
+++ category-people.php
@@ -4,74 +4,75 @@ $lab_hard_slug = 'people';
 
 get_header();
 get_sidebar();
+//note: the outer for loop is required because
+//ordering by meta data (such as lab position)
+//does not work for users. We instead perform
+//a separate query for each position level so 
+//members appear in the right order.
+$users = array();
+for ($pos = 1; $pos <= 8; $pos++) {		
+	$users = array_merge($users, get_users( array (
+		'meta_key' => '_lab_position',
+		'meta_value' => "$pos",
+	) ) );
+}
 
-$users = get_users( array (
-	'orderby' => 'meta_value_num',
-	'order' => 'ASC',
-	'meta_key' => '_lab_position',
-	'nopaging' => 'true',
-) );
 
 ?>
 
-<div class = "container">
-	
+<div class = "container"><div class = "row">
 	<?php
 	$mem_num = 0;
 	foreach ($users as $user) {
 		$meta_user = new Lab_User($user);		
 		?>
-		<div class = "labMemberSummary col-md-4 col-sm-4 col-xs-6" 
+		<div class = "labMemberSummary col-sm-4 col-xs-12" 
 		id = "memSum<?php echo $mem_num; ?>">
 			<h3><?php echo $meta_user->name; ?></h3>
 			<h4><?php echo $meta_user->pos; ?></h4>
 			<h6><a>More ...</a></h6>
 			<img src = "<?php echo $meta_user->pic; ?>" width = "140 px" >
-		</div>
-		
-		
+		</div>				
 		<?php	
 		$mem_num++;
+		if ($mem_num % 3 == 0) echo '</div><div class = "row">';
 	}
 	?>
 <!--close container-->
-</div>
+</div></div>
 
 <div id = "popupBckgd"> </div>
-	<?php
-		//restart the loop
-		$mem_num = 0;
-		foreach ($users as $user) {
-			$meta_user = new Lab_User($user);
-			?>
-			<div class = "popup container labMemberFull" id = "labMemFull<?php echo $mem_num; ?>">
-				<div class = "popupHeader row">
-					<div class = "col-xs-6">
-						<h1><?php echo $meta_user->name; ?></h3>
-						<h2><?php echo $meta_user->pos; ?></h4>
-					</div>
-					<img src = "<?php echo $meta_user->pic; ?>" 
-					class = "col-xs-6 labMemberFullPic" width = "140 px" >
-				</div>
-				<div class = "row">
-					<p class = "bio col-xs-12"><?php echo $meta_user->bio; ?></p>
-				</div>
-				
-				<?php if ($meta_user->pubs_bool): ?>
-					<div class = "row">
-						<h4 class = "col-xs-12">Publications</h4>
-						<div class = "col-xs-12">
-							<?php echo $meta_user->pubs_str; ?>
-						</div>
-					</div>
-				<?php endif; ?>
-			</div>
-			<?php
-			$mem_num++;
-		}	
-
-	?>
 <?php
+	//restart the loop
+	$mem_num = 0;
+	foreach ($users as $user) {
+		$meta_user = new Lab_User($user);
+		?>
+		<div class = "popup container labMemberFull" id = "labMemFull<?php echo $mem_num; ?>">
+			<div class = "popupHeader row">
+				<div class = "col-xs-6">
+					<h1><?php echo $meta_user->name; ?></h3>
+					<h2><?php echo $meta_user->pos; ?></h4>
+				</div>
+				<img src = "<?php echo $meta_user->pic; ?>" 
+				class = "col-xs-6 labMemberFullPic" width = "140 px" >
+			</div>
+			<div class = "row">
+				<p class = "bio col-xs-12"><?php echo $meta_user->bio; ?></p>
+			</div>
+			
+			<?php if ($meta_user->pubs_bool): ?>
+				<div class = "row">
+					<h4 class = "col-xs-12">Publications</h4>
+					<div class = "col-xs-12">
+						<?php echo $meta_user->pubs_str; ?>
+					</div>
+				</div>
+			<?php endif; ?>
+		</div>
+		<?php
+		$mem_num++;
+	}	
 
 get_footer();
 
